520b3d6de29e93b3dac876596b0c9f194ab77814
pavan <pavankumarreddy155@gmail.com>
2025-02-20 19:46:26 +0530

    [Pavan] Add: Python Script for Calculating Daily wage Of an Employee

--- emp.py
+++ emp.py
@@ -1,13 +1,12 @@
-import random
 
-def check_attendance():
-    print('Welcome to Employee Wage Program on Master Branch')
-    attendance = random.choice([0, 1])  # Correct way to randomly pick 0 or 1
-    if attendance == 1:
-        print('The Employee is Present')
-    else:
-        print('The Employee is Absent')
 
-if __name__ == '__main__':
-    check_attendance()
-    print('Welcome to Employee Wage Problem')
+def calculate_daily_wage(hours_worked,hour_rate):
+   return hours_worked * hour_rate
+
+
+hours=float(input('Enter the hours worked by Employee: '))
+hour_rate=8
+
+total_wage=calculate_daily_wage(hours,hour_rate)
+
+print(f'The Employee daily wage for hours worked {hours} is {total_wage} ')
