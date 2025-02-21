76d173a5abb6150bd6dfca047cb1298fed5031fb
pavan <pavankumarreddy155@gmail.com>
2025-02-21 11:18:32 +0530

    [Pavan] Add:Python Script for Employee Wage and Attendance Using Switch Case

--- emp.py
+++ emp.py
@@ -1,30 +1,27 @@
-
 import random
 
 def calculate_daily_wage(hours_worked, hour_rate):
     return hours_worked * hour_rate
 
-def check_attendance():
-    attendance = random.choice([0, 1, 2])  
-    if attendance == 1:
-        print('The Employee is Present,Employee is a Full Time Employee')
-        hours_worked=8
-        total_wage = calculate_daily_wage(hours_worked, hour_rate)
-        print(f'The Employee daily wage for hours worked {hours_worked} is {total_wage}')
-    elif attendance == 2:
-        print('The Employee is Present,Employee is a Part-Time Employee')
-        hours_worked=4
-        total_wage = calculate_daily_wage(hours_worked, hour_rate)
-        print(f'The Employee daily wage for hours worked {hours_worked} is {total_wage}')
+def check_attendance(hour_rate):
+    attendance = random.choice([0, 1, 2]) 
 
-    else:
-        print('The Employee is Absent , The Working Hours is Zero ')
+    match attendance:
+        case 1:
+            print('The Employee is Present, Employee is a Full-Time Employee')
+            hours_worked = 8
+        case 2:
+            print('The Employee is Present, Employee is a Part-Time Employee')
+            hours_worked = 4
+        case _:
+            print('The Employee is Absent, The Working Hours is Zero')
+            return 
 
+    total_wage = calculate_daily_wage(hours_worked, hour_rate)
+    print(f'The Employee daily wage for hours worked {hours_worked} is {total_wage}')
 
 hour_rate = 20
 
 if __name__ == '__main__':
     print('Welcome to Employee Wage Problem')
-    check_attendance()
-
-
+    check_attendance(hour_rate)
