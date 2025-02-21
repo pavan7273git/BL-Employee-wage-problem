fb855efb412e04c734d8e4f24d49d4c3401b72c9
pavan <pavankumarreddy155@gmail.com>
2025-02-21 11:00:14 +0530

    [Pavan] Add: Python Script for Calculating Part-time Employee Wage

--- emp.py
+++ emp.py
@@ -1,12 +1,30 @@
 
+import random
 
-def calculate_daily_wage(hours_worked,hour_rate):
-   return hours_worked * hour_rate
+def calculate_daily_wage(hours_worked, hour_rate):
+    return hours_worked * hour_rate
+
+def check_attendance():
+    attendance = random.choice([0, 1, 2])  
+    if attendance == 1:
+        print('The Employee is Present,Employee is a Full Time Employee')
+        hours_worked=8
+        total_wage = calculate_daily_wage(hours_worked, hour_rate)
+        print(f'The Employee daily wage for hours worked {hours_worked} is {total_wage}')
+    elif attendance == 2:
+        print('The Employee is Present,Employee is a Part-Time Employee')
+        hours_worked=4
+        total_wage = calculate_daily_wage(hours_worked, hour_rate)
+        print(f'The Employee daily wage for hours worked {hours_worked} is {total_wage}')
+
+    else:
+        print('The Employee is Absent , The Working Hours is Zero ')
 
 
-hours=float(input('Enter the hours worked by Employee: '))
-hour_rate=8
+hour_rate = 20
 
-total_wage=calculate_daily_wage(hours,hour_rate)
+if __name__ == '__main__':
+    print('Welcome to Employee Wage Problem')
+    check_attendance()
 
-print(f'The Employee daily wage for hours worked {hours} is {total_wage} ')
+
