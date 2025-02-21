55e93c23ecc94c3b8badf8370dc64ea65592f70c
pavan <pavankumarreddy155@gmail.com>
2025-02-21 15:16:04 +0530

    [Pavan] Add:Python Script for Calculating the Monthly Wage

--- emp.py
+++ emp.py
@@ -15,13 +15,24 @@ def check_attendance(hour_rate):
             hours_worked = 4
         case _:
             print('The Employee is Absent, The Working Hours is Zero')
-            return 
+            return 0
 
     total_wage = calculate_daily_wage(hours_worked, hour_rate)
     print(f'The Employee daily wage for hours worked {hours_worked} is {total_wage}')
+    return total_wage
 
 hour_rate = 20
 
 if __name__ == '__main__':
     print('Welcome to Employee Wage Problem')
-    check_attendance(hour_rate)
+    total_monthly_wage =0
+
+    for i in range(1,21):
+        print(f'Day : {i}')
+        daily_wage=check_attendance(hour_rate)
+        total_monthly_wage +=daily_wage
+
+    print(f'The total Monthly wage is: {total_monthly_wage}')
+
+
+
