5e521d399457e22cd0d49e189506766dc3f6a79d
pavan7273git <148039179+pavan7273git@users.noreply.github.com>
2025-02-21 10:42:37 +0530

    [

--- emp.py
+++ emp.py
@@ -1,25 +1,21 @@
 
 import random
 
+def calculate_daily_wage(hours_worked, hour_rate):
+    return hours_worked * hour_rate
+
 def check_attendance():
-    print('Welcome to Employee Wage Program on Master Branch')
     attendance = random.choice([0, 1])  
     if attendance == 1:
         print('The Employee is Present')
+        total_wage = calculate_daily_wage(hours_worked, hour_rate)
+        print(f'The Employee daily wage for hours worked {hours_worked} is {total_wage}')
     else:
         print('The Employee is Absent')
 
+hours_worked = 8
+hour_rate = 20
+
 if __name__ == '__main__':
-    check_attendance()
     print('Welcome to Employee Wage Problem')
-
-def calculate_daily_wage(hours_worked,hour_rate):
-   return hours_worked * hour_rate
-
-
-hours_worked=8
-hour_rate=20
-
-total_wage=calculate_daily_wage(hours_worked,hour_rate)
-
-print(f'The Employee daily wage for hours worked {hours_worked} is {total_wage} ')
+    check_attendance()
